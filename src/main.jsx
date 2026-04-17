012eeff9fd81ac715cec26cfd5f0733c62ca7671
yeri2306 <yeri2306@gmail.com>
2026-04-17 14:17:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,7 @@
-import React from 'react' import ReactDOM from 'react-dom/client' import App from './store-staffing-app' ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode><App/></React.StrictMode> )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './store-staffing-app'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App/></React.StrictMode>
+)
